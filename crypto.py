27b0b5e5437e46a5e0cc0d1f8c2e5b08e6f55a27
ssaiganesh <shankarsaiganesh@gmail.com>
2021-02-27 15:01:17 +0800

    Crypto and Stock prediction apps added

---
+++ crypto.py
@@ -0,0 +1,12 @@
+#Reference: https://github.com/FinPython/socialmediaposts/blob/main/13-streamlit-50-crypto
+#More improvements upcoming
+
+import streamlit as st
+import pandas_datareader.data as web
+from datetime import datetime as dt, timedelta as td
+
+st.title('Crypto Tracker')
+opts = st.selectbox('Select Pair', 
+    ('BTC-USD', 'ETH-USD', 'BNB-USD','XRP-USD','LINK-USD','DOGE-USD','EOS-USD','BSV-USD'))
+prices = web.get_data_yahoo(opts, start=dt.now() - td(days=365), end=dt.now())
+st.line_chart(prices)
